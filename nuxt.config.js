795d905c0c0346229327e925accef601b13ad05c
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2018-01-04 17:07:34 -0600

    Updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,7 @@ module.exports = {
         title: 'Irfaan Khalid',
         meta: [
             { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: 'My personal website built in Vue using Nuxt.js.' }
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
         ],
         link: [
             { rel: 'shortcut icon', type: 'image/png', href: '/favicon.png' }
